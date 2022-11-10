f0053cdcbcd26d70817f87857fee34758eb37dd6
norcess <hwang2022@elam.co.uk>
2022-11-10 17:47:22 +0000

    camera

--- 3d platform/Assets/PlayerController.cs
+++ 3d platform/Assets/PlayerController.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    float maxSpeed = 7.0f;
+    public float maxSpeed;
+    public float normalSpeed = 3.0f;
+    public float sprintSpeed = 7.0f;
+
+
     float rotation = 0.0f;
     float camRotation = 0.0f;
     float rotationSpeed = 2.0f;
@@ -21,6 +25,7 @@ public class PlayerController : MonoBehaviour
     {
         cam = GameObject.Find("Main Camera");
         myRigidbody = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
 
@@ -33,13 +38,22 @@ public class PlayerController : MonoBehaviour
             myRigidbody.AddForce(transform.up * jumpForce);
         }
 
-       Vector3 newVelocity = transform.forward * Input.GetAxis("Vertical") * maxSpeed;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            maxSpeed = sprintSpeed;
+        }else
+        {
+            maxSpeed = normalSpeed;
+        }
+
+       Vector3 newVelocity = transform.forward * Input.GetAxis("Vertical") * (maxSpeed) + (transform.right * Input.GetAxis("Horizontal") * maxSpeed);
        myRigidbody.velocity = new Vector3(newVelocity.x, myRigidbody.velocity.y, newVelocity.z);
 
         rotation = rotation + Input.GetAxis("Mouse X") * rotationSpeed;
         transform.rotation = Quaternion.Euler(new Vector3(0.0f, rotation, 0.0f));
 
         camRotation = camRotation + Input.GetAxis("Mouse Y") * camRotationSpeed;
+        camRotation = Mathf.Clamp(camRotation, -40.0f, 40.0f);
         cam.transform.localRotation = Quaternion.Euler(new Vector3(camRotation, 0.0f, 0.0f));
     }
 }
